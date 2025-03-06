08216e1658a9c28ef5ccb290641afbde3f085758
stha <stha@aprixon.de>
2025-03-06 08:45:31 +0100

    pinie colada demo

--- src/queries/todos.ts
+++ src/queries/todos.ts
@@ -2,19 +2,17 @@ import { defineQuery } from '@pinia/colada'
 
 export const useTodos = defineQuery({
   key: ['todos'],
-  // query: () => [
-  //   { "id": "1", "content": "a placeholder todo" },
-  //   { "id": "2", "content": "another placeholder todo" }
-  // ],
   query: () => fetch('http://localhost:3000/todos').then((res) => res.json()),
-  staleTime: 2 * 1000,
-
+  staleTime: 3 * 1000,
 })
 
 
 
 
-
+// query: () => [
+//   { "id": "1", "content": "a placeholder todo" },
+//   { "id": "2", "content": "another placeholder todo" }
+// ],
 
 
 
